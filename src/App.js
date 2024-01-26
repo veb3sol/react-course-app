84a261f55f89f3fb90c91d4186a34660b0c77580
Ruslan <veb3solidity@gmail.com>
2024-01-26 11:10:07 +0200

    less1_2_16

--- src/App.js
+++ src/App.js
@@ -30,9 +30,9 @@ function App() {
     <div>
       <h1>Начнем изучение React</h1>
       <p>Всем привет!</p>
-      <CostItem date={costs[0].date} description={costs[0].description} amount={costs[0].amount}></CostItem>
-      <CostItem date={costs[1].date} description={costs[1].description} amount={costs[1].amount}></CostItem>
-      <CostItem date={costs[2].date} description={costs[2].description} amount={costs[2].amount}></CostItem>
+      <CostItem date={costs[0].date} description={costs[0].description} amount={costs[0].amount} />
+      <CostItem date={costs[1].date} description={costs[1].description} amount={costs[1].amount} />
+      <CostItem date={costs[2].date} description={costs[2].description} amount={costs[2].amount} />
     </div>
   );
 }
