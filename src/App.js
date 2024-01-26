7456bdb2f57abd2a3955c59efb9db464673c0867
Ruslan <veb3solidity@gmail.com>
2024-01-26 11:32:23 +0200

    less1_2_17 exercise1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import CostItem from "./components/CostItem";
+import Costs from "./components/Costs";
 
 
 function App() {
@@ -30,9 +30,7 @@ function App() {
     <div>
       <h1>Начнем изучение React</h1>
       <p>Всем привет!</p>
-      <CostItem date={costs[0].date} description={costs[0].description} amount={costs[0].amount} />
-      <CostItem date={costs[1].date} description={costs[1].description} amount={costs[1].amount} />
-      <CostItem date={costs[2].date} description={costs[2].description} amount={costs[2].amount} />
+      <Costs costs={costs}/>
     </div>
   );
 }
--- src/components/CostItem.js
+++ src/components/CostItem.js
@@ -2,9 +2,6 @@ import './CostItem.css';
 import CostDate from './CostDate';
 
 function CostItem(props){
-
-   
-
     return (<div className="cost-item">
             <CostDate date={props.date}/>
             <div className='cost-item__description'>
