9fc62a7f8b9092a7911cda7f64e8bf2caae739b8
Ruslan <veb3solidity@gmail.com>
2024-02-08 13:40:33 +0200

    end 01_03

--- src/components/Costs/CostItem.js
+++ src/components/Costs/CostItem.js
@@ -1,30 +1,18 @@
 import "./CostItem.css";
 import CostDate from "./CostDate";
 import Card from "../UI/Card";
-import React, { useState } from "react";
+
 
 function CostItem(props) {
 
-  // useState() -- вызывается в компоненте до ретурна, не может вызываться в других функциях
-  // хук вызывает перерисовку этого компонента при изменении данных
-  // параметр - начальное значение
-
-  const [description, setDescription] = useState(props.description)
-  // description - текущее значение состояния
-  // setDescription - функция для изменения переменной
-
-
-  const changeDescriptionHandler = () => {
-    setDescription('Комбайн')
-  }
+  
   return (
     <Card className="cost-item">
       <CostDate date={props.date} />
       <div className="cost-item__description">
-        <h2>{description}</h2>
+        <h2>{props.description}</h2>
         <div className="cost-item__price">${props.amount}</div>
       </div>
-      <button onClick={changeDescriptionHandler}>Изменить описание</button>
     </Card>
   );
 }
--- src/components/Costs/Costs.js
+++ src/components/Costs/Costs.js
@@ -1,10 +1,23 @@
 import CostItem from "./CostItem";
+import CostsFilter from "./CostsFilter";
 import Card from "../UI/Card";
 import "./Costs.css";
+import { useState } from "react";
 
 function Costs(props) {
+  const years = {
+    y: 2021
+  }
+  const[myYear, setMyYear] = useState(years.y)
+  const onChangeYear = (year) => {
+    setMyYear(year)
+    years.y = year
+    console.log(years)
+  }
   return (
-    <Card className="costs">
+    
+      <Card className="costs">
+        <CostsFilter onChangeYear={onChangeYear} year={myYear}/>
       <CostItem
         date={props.costs[0].date}
         description={props.costs[0].description}
@@ -21,6 +34,8 @@ function Costs(props) {
         amount={props.costs[2].amount}
       />
     </Card>
+    
+    
   );
 }
 
